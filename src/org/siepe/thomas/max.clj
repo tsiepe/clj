422284172a3efd121c66778695f7845020f08dd5
Thomas Siepe <tsiepe@gmx.net>
2020-04-10 16:23:30 +0200

    Do max properly.

--- src/org/siepe/thomas/max.clj
+++ src/org/siepe/thomas/max.clj
@@ -1,12 +1,5 @@
 (ns org.siepe.thomas.max)
 
 (defn max [& values]
-  (loop [candidate (first values)
-         rest-of-list values]
-    (if-not (> (count rest-of-list) 1)
-      candidate
-      (recur (if (> candidate (second rest-of-list))
-               candidate
-               (second rest-of-list))
-             (rest rest-of-list)))))
+  (reduce #(if (> %1 %2) %1 %2) values))
 
